9337466b1d42923550b5ff40edac26949d166099
Jaiber Herrere <jaiberh06@gmail.com>
2024-10-05 10:13:55 -0500

    Ajuste final de modelos

--- prueba/app/admin.py
+++ prueba/app/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import Estudiante
-from .models import Inscripcion
-from .models import Profesor
-from .models import Materia
+from .models import Site
+from .models import Career
+from .models import Journey
+from .models import Student
+from .models import Teacher
+from .models import Subject
+from .models import Registration
 # Register your models here.
-admin.site.register(Estudiante)
-admin.site.register(Inscripcion)
-admin.site.register(Profesor)
-admin.site.register(Materia)
+admin.site.register(Site)
+admin.site.register(Career)
+admin.site.register(Journey)
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Subject)
+admin.site.register(Registration)
--- prueba/app/models.py
+++ prueba/app/models.py
@@ -1,27 +1,55 @@
 from django.db import models
 
 # Create your models here.
-class Estudiante (models.Model):
-    nombre =  (models.CharField(max_length= 250))
-    apellido =(models.CharField(max_length=250))
-    fechaNacimiento = (models.DateField)
-    correoElectronico = (models.EmailField)
-    
 
+class SiteType(models.TextChoices):
+    SEDE_A = 'SA', 'Sede A'
+    SEDE_B = 'SB', 'Sede B'
 
-class Profesor (models.Model):
-    nombre = (models.CharField(max_length=250))
-    apellido = (models.CharField(max_length= 250))
-    departamento = (models.CharField(max_length= 250))
+class Site(models.Model):
+    type = models.CharField(
+        max_length=2,
+        choices=SiteType.choices,
+        default=SiteType.SEDE_A,
+        verbose_name='Tipo de sede'
+    )
 
-class Materia (models.Model):
-    nombre =(models.CharField(max_length= 250))
-    descripcion =(models.TextField)
-    numeroCreditos = (models.IntegerField)
-    idProfesor = (models.ForeignKey(Profesor, on_delete= models.CASCADE))
+class Career(models.Model):
+    name = models.CharField(max_length=250, verbose_name='Nombre')
 
-class Inscripcion (models.Model):
-    idEstudiante = (models.ForeignKey(Estudiante, on_delete=models.CASCADE))
-    idMateria = (models.ForeignKey(Materia,on_delete=models.CASCADE))
-    fechaInscripcion =(models.DateField)
-    
+class JourneyType(models.TextChoices):
+    DIURNA = 'DI', 'Diurna'
+    NOCTURNA = 'NO', 'Nocturna'
+
+class Journey(models.Model):
+    type = models.CharField(
+        max_length=2,
+        choices=JourneyType.choices,
+        default=JourneyType.DIURNA,
+        verbose_name='Tipo de jornada'
+    )
+
+class Student(models.Model):
+    name = models.CharField(max_length= 250, verbose_name='Nombre')
+    lastName = models.CharField(max_length=250, verbose_name='Apellido')
+    birthDate = models.DateField(verbose_name='Fecha de nacimiento')
+    email = models.EmailField(verbose_name='Correo electrónico')
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=250, verbose_name='Nombre')
+    lastname = models.CharField(max_length= 250, verbose_name='Apellido')
+    state = models.CharField(max_length= 250, verbose_name='Departamento')
+
+class Subject(models.Model):
+    name = models.CharField(max_length= 250, verbose_name='Nombre')
+    description = models.TextField(verbose_name='Descripción')
+    numberOfCredits = models.IntegerField(verbose_name='Número de créditos')
+    teacherId = models.ForeignKey(Teacher, on_delete= models.CASCADE, verbose_name='Id del profesor')
+
+class Registration(models.Model):
+    studentId = models.ForeignKey(Student, on_delete=models.CASCADE, verbose_name='Estudiante')
+    subjectId = models.ForeignKey(Subject,on_delete=models.CASCADE, verbose_name='Materia')
+    siteId = models.ForeignKey(Site, on_delete=models.CASCADE, verbose_name='Sede')
+    careerId = models.ForeignKey(Career, on_delete=models.CASCADE, verbose_name='Carrera')
+    journeyId = models.ForeignKey(Journey, on_delete=models.CASCADE, verbose_name='Jornada')
+    registrationDate = models.DateField(verbose_name='Fecha de registro')
